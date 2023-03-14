2810290772b0d59c678c8b29ace41917dc1e646f
Marius <marius.havnaas@drift.oslo.kommune.no>
2023-03-14 13:52:01 +0100

    Added teplate file.

---
+++ src/components/template.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NewItem",
+});
+</script>
+
+<style scoped>
+#div {
+  color: #000;
+}
+</style>
